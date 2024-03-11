1574b7f01134e7e8197b3111197bf703ead08cab
Bilal <bm@fabricdata.com>
2024-03-11 21:49:02 +0300

    updated projects content

--- app/content/projects.js
+++ app/content/projects.js
@@ -2,41 +2,89 @@ export const projects = [
     {
         title: 'Proje 1',
         image: '/images/about.jpg',
-        slug: 'proje-1'
+        slug: 'proje-1',
+        shortDescription: 'Short description of Proje 1',
+        longDescription: 'Long description of Proje 1',
+        status: 'Finished',
+        startDate: '2023-01-01',
+        deliveryDate: '2023-03-01',
+        projectImages: ['/images/about.jpg', '/images/projects.png']
     },
     {
         title: 'Proje 2',
         image: '/images/projects.png',
-        slug: 'proje-2'
+        slug: 'proje-2',
+        shortDescription: 'Short description of Proje 2',
+        longDescription: 'Long description of Proje 2',
+        status: 'Still Working On',
+        startDate: '2023-02-01',
+        deliveryDate: '2023-04-01',
+        projectImages: ['/images/projects.png', '/images/about.jpg']
     },
     {
         title: 'Proje 3',
         image: '/images/about.jpg',
-        slug: 'proje-3'
+        slug: 'proje-3',
+        shortDescription: 'Short description of Proje 3',
+        longDescription: 'Long description of Proje 3',
+        status: 'Finished',
+        startDate: '2023-01-15',
+        deliveryDate: '2023-03-15',
+        projectImages: ['/images/about.jpg', '/images/projects.png']
     },
     {
         title: 'Proje 4',
         image: '/images/projects.png',
-        slug: 'proje-4'
+        slug: 'proje-4',
+        shortDescription: 'Short description of Proje 4',
+        longDescription: 'Long description of Proje 4',
+        status: 'Still Working On',
+        startDate: '2023-02-15',
+        deliveryDate: '2023-04-15',
+        projectImages: ['/images/projects.png', '/images/about.jpg']
     },
     {
         title: 'Proje 5',
         image: '/images/about.jpg',
-        slug: 'proje-5'
+        slug: 'proje-5',
+        shortDescription: 'Short description of Proje 5',
+        longDescription: 'Long description of Proje 5',
+        status: 'Finished',
+        startDate: '2023-03-01',
+        deliveryDate: '2023-05-01',
+        projectImages: ['/images/about.jpg', '/images/projects.png']
     },
     {
         title: 'Proje 6',
         image: '/images/projects.png',
-        slug: 'proje-6'
+        slug: 'proje-6',
+        shortDescription: 'Short description of Proje 6',
+        longDescription: 'Long description of Proje 6',
+        status: 'Still Working On',
+        startDate: '2023-03-15',
+        deliveryDate: '2023-05-15',
+        projectImages: ['/images/projects.png', '/images/about.jpg']
     },
     {
         title: 'Proje 7',
         image: '/images/about.jpg',
-        slug: 'proje-7'
+        slug: 'proje-7',
+        shortDescription: 'Short description of Proje 7',
+        longDescription: 'Long description of Proje 7',
+        status: 'Finished',
+        startDate: '2023-04-01',
+        deliveryDate: '2023-06-01',
+        projectImages: ['/images/about.jpg', '/images/projects.png']
     },
     {
         title: 'Proje 8',
         image: '/images/projects.png',
-        slug: 'proje-8'
+        slug: 'proje-8',
+        shortDescription: 'Short description of Proje 8',
+        longDescription: 'Long description of Proje 8',
+        status: 'Still Working On',
+        startDate: '2023-04-15',
+        deliveryDate: '2023-06-15',
+        projectImages: ['/images/projects.png', '/images/about.jpg']
     }
-]
+];
